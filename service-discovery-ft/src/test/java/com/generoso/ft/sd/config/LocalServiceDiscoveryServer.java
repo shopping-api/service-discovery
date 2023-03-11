929e37cf991887877a3322fefacc7a6e93d17329
Mauricio Generoso <mauro_generoso@hotmail.com>
2023-03-11 17:22:41 +0000

    upgrade to spring 3 in ft and eureka server to 4

--- service-discovery-ft/src/test/java/com/generoso/ft/sd/config/LocalServiceDiscoveryServer.java
+++ service-discovery-ft/src/test/java/com/generoso/ft/sd/config/LocalServiceDiscoveryServer.java
@@ -1,13 +1,12 @@
 package com.generoso.ft.sd.config;
 
 import com.generoso.sd.ServiceDiscoveryServerApplication;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Profile("local")
 @Configuration
--- service-discovery-ft/src/test/java/com/generoso/ft/sd/steps/PrivateStepDefinitions.java
+++ service-discovery-ft/src/test/java/com/generoso/ft/sd/steps/PrivateStepDefinitions.java
@@ -1,7 +1,6 @@
 package com.generoso.ft.sd.steps;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.generoso.ft.sd.client.RequestTemplate;
 import com.generoso.ft.sd.client.model.Endpoint;
