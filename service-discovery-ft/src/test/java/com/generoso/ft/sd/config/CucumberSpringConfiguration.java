b466eda9b1b8578414e5ac1b83ec91ab9bf8e099
Mauricio Generoso <mauricio.generoso@hotmail.com>
2024-06-24 19:54:29 +0100

    refs #101: Remove guava and YamlFileApplicationContextInitializer

--- service-discovery-ft/src/test/java/com/generoso/ft/sd/config/CucumberSpringConfiguration.java
+++ service-discovery-ft/src/test/java/com/generoso/ft/sd/config/CucumberSpringConfiguration.java
@@ -1,12 +1,12 @@
 package com.generoso.ft.sd.config;
 
-import com.generoso.ft.sd.YamlFileApplicationContextInitializer;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
+@SpringBootTest
 @ContextConfiguration(
-    classes = {TestConfiguration.class},
-    initializers = YamlFileApplicationContextInitializer.class)
+    classes = {TestConfiguration.class})
 @CucumberContextConfiguration
 public class CucumberSpringConfiguration {
 }
