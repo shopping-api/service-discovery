99988666629cfb8247ad4a8a3cf967febc4bbe4a
Mauricio Generoso <mauricio.generoso@hotmail.com>
2024-06-28 22:36:07 +0100

    refs #104: Create main class for FT

---
+++ sd-ft/src/test/java/com/generoso/ft/sd/Main.java
@@ -0,0 +1,16 @@
+package com.generoso.ft.sd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var testArgs = new String[]{
+            "--tags", "not @ignore",
+            "--plugin", "pretty",
+            "--plugin", "html:sd-ft/reports/report.html",
+            "--glue", "com.generoso.ft.sd",
+            "classpath:features"
+        };
+
+        io.cucumber.core.cli.Main.main(testArgs);
+    }
+}
